f15a4e7d0e17ea710e56dd185cb4ac7713570735
840288 <840288@SAIT225697IT.ACDM.DS.SAIT.CA>
2021-07-15 10:51:00 -0600

    Second Commit Completed the lab, converted RoleDB and UserDb to JPA format and also had to change some values in the jsp for roleId and roleName. Also made changes to UserService insert.

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -19,8 +19,9 @@ public class UserService {
     }
     
     public void insert(String email, boolean activity, String first_name, String last_name, String password, Role role) throws Exception{
-        User user = new User(email, activity, first_name, last_name, password, role);
+        User user = new User(email, activity, first_name, last_name, password);
         UserDB userDB = new UserDB();
+        user.setRole(role);
         userDB.insert(user);
     }
     
