f207ebe249a3c94c856c05becaef4bee26c27761
840288 <840288@SAIT225697IT.ACDM.DS.SAIT.CA>
2021-07-15 09:32:48 -0600

    First Commit Added persistence unit and eclipse jpa, deleted connection pool class. Changed DBUtil class.

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.Persistence;
 
 public class DBUtil {
     private static final EntityManagerFactory emf =
-        Persistence.createEntityManagerFactory("Lab7AltPU");
+        Persistence.createEntityManagerFactory("Lab09PU");
 
     public static EntityManagerFactory getEmFactory() {
         return emf;
